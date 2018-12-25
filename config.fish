6bc30b488d7d8d8ac5ac5090736c523ddeac0730
dong guo <dong.guo@mobilus.co.jp>
2018-12-25 17:27:11 +0900

    update

--- config.fish
+++ config.fish
@@ -120,9 +120,3 @@ powerline-setup
 function dired
   emacsclient -e "(dired \"$PWD\")"
 end
-
-function cde
-  emacsclient -e "(return-current-working-directory-to-shell)" | sed 's/^"\(.*\)"$/\1/' | read EMACS_CWD
-  echo "chdir to $EMACS_CWD"
-  cd "$EMACS_CWD"
-end
